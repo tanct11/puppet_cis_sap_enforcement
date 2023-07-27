93af4facd94f1d3d09c23217eca0d2bb20fa7366
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-07-27 20:52:41 +0800

    Update ensure_auditing_for_processes_that_start_prior_to_auditd_is_enabled.pp

--- manifests/rules/ensure_auditing_for_processes_that_start_prior_to_auditd_is_enabled.pp
+++ manifests/rules/ensure_auditing_for_processes_that_start_prior_to_auditd_is_enabled.pp
@@ -4,7 +4,6 @@
 #
 class secure_linux_cis::rules::ensure_auditing_for_processes_that_start_prior_to_auditd_is_enabled {
   Class['secure_linux_cis::rules::ensure_auditing_for_processes_that_start_prior_to_auditd_is_enabled']
-  ~> Class['secure_linux_cis']
   exec { 'Ensure auditing for processes that start prior to auditd is enabled':
     command   => "/usr/sbin/grubby --update-kernel ALL --args 'audit=1'",  #lint:ignore:140chars
     unless    => "/usr/bin/find /boot -type f -name 'grubenv' -exec grep -P 'kernelopts=([^#\n\r]+\h+)?(audit=1)' {} \;",  #lint:ignore:140chars
