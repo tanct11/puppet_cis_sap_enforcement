d497f71cd5e9cc1dc48986d45112eefdd2b54fc3
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-07-27 15:40:38 +0800

    Update ensure_permissions_on_bootloader_config_are_configured.pp

--- manifests/rules/ensure_permissions_on_bootloader_config_are_configured.pp
+++ manifests/rules/ensure_permissions_on_bootloader_config_are_configured.pp
@@ -3,7 +3,13 @@
 # @summary Ensure permissions on bootloader config are configured 
 #
 class secure_linux_cis::rules::ensure_permissions_on_bootloader_config_are_configured {
-  file { $secure_linux_cis::grub_config_files:
+  file { '/boot/grub2/grub.cfg':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => 'og-rwx',
+  }
+    file { '/boot/grub2/user.cfg':
     ensure => file,
     owner  => 'root',
     group  => 'root',
