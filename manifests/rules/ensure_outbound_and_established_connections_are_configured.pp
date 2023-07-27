1bfebbf571fbf82ddae072b5e02591f020e95a46
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-07-27 17:03:34 +0800

    Update ensure_outbound_and_established_connections_are_configured.pp

--- manifests/rules/ensure_outbound_and_established_connections_are_configured.pp
+++ manifests/rules/ensure_outbound_and_established_connections_are_configured.pp
@@ -56,36 +56,7 @@ class secure_linux_cis::rules::ensure_outbound_and_established_connections_are_c
         tag    => 'cis_firewall_pre',
       }
     }
-    'nftables': {
-      # Create default drop policy for nftables
-      # nftables::rule { 'INPUT-tcp':
-      #   content => 'ip protocol tcp ct state established accept',
-      # }
-
-      # nftables::rule { 'INPUT-tcp-ssh':
-      #   content => 'tcp dport 22 accept',
-      # }
-
-      # nftables::rule { 'INPUT-udp':
-      #   content => 'ip protocol udp ct state established accept',
-      # }
-
-      # nftables::rule { 'INPUT-icmp':
-      #   content => 'ip protocol icmp ct state established accept',
-      # }
-
-      # nftables::rule { 'OUTPUT-tcp':
-      #   content => 'ip protocol tcp ct state new,related,established accept',
-      # }
-
-      # nftables::rule { 'OUTPUT-udp':
-      #   content => 'ip protocol udp ct state new,related,established accept',
-      # }
-
-      # nftables::rule { 'OUTPUT-icmp':
-      #   content => 'ip protocol icmp ct state new,related,established accept',
-      # }
-    }
+   
     default: { file('Need iptables or nftables firewall.') }
   }
 }
