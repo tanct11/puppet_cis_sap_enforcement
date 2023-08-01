27a5f114661a295c1d31ce20aef6822fd9b89538
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-01 18:55:07 +0800

    Update ensure_only_strong_ciphers_are_used.pp

--- manifests/rules/ensure_only_strong_ciphers_are_used.pp
+++ manifests/rules/ensure_only_strong_ciphers_are_used.pp
@@ -3,15 +3,11 @@
 # @summary Ensure only strong Ciphers are used 
 #
 class secure_linux_cis::rules::ensure_only_strong_ciphers_are_used {
-  include secure_linux_cis::sshd_service
-
-  $ciphers_array = join($secure_linux_cis::approved_ciphers, ',')
 
   file_line { 'ssh ciphers':
     ensure => 'present',
     path   => '/etc/ssh/sshd_config',
-    line   => "Ciphers ${ciphers_array}",
+    line   => "Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr",
     match  => '^#?Ciphers',
-    notify => Class['secure_linux_cis::sshd_service'],
   }
 }
