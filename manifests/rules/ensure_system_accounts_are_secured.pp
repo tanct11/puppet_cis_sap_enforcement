f0d1ab78434eca45ab2b1711276035cc1ddba7d8
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-01 21:14:13 +0800

    Update ensure_system_accounts_are_secured.pp

--- manifests/rules/ensure_system_accounts_are_secured.pp
+++ manifests/rules/ensure_system_accounts_are_secured.pp
@@ -3,39 +3,18 @@
 # @summary Ensure system accounts are secured 
 #
 class secure_linux_cis::rules::ensure_system_accounts_are_secured {
-  # unless $facts['insecure_system_accounts'] {
-  #   warning('Insecure system accounts are present.')
-  # }
 
-  file { '/root/scripts/login_nologin_system_accounts.sh':
-    ensure => file,
-    source => 'puppet:///modules/secure_linux_cis/login_nologin_system_accounts.sh',
-    mode   => 'u+x',
-  }
-  -> file { '/root/scripts/audit_nologin_system_accounts.sh':
-    ensure => file,
-    source => 'puppet:///modules/secure_linux_cis/audit_nologin_system_accounts.sh',
-    mode   => 'u+x',
-  }
-  -> exec { 'Set all system accounts to a non login shell':
-    command => '/root/scripts/login_nologin_system_accounts.sh',
-    unless  => 'test -z `/root/scripts/audit_nologin_system_accounts.sh`',
+
+
+  exec { 'Set all system accounts to a non login shell':
+    command => 'awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"' && $7!="'"$(which nologin)"'" && $7!="/bin/false" && $7!="/usr/bin/false") {print $1}' /etc/passwd | while read -r user; do usermod -s "$(which nologin)" "$user"; done',
     path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
   }
 
-  file { '/root/scripts/lock_unlocked_system_accounts.sh':
-    ensure => file,
-    source => 'puppet:///modules/secure_linux_cis/lock_unlocked_system_accounts.sh',
-    mode   => 'u+x',
-  }
-  -> file { '/root/scripts/audit_unlocked_system_accounts.sh':
-    ensure => file,
-    source => 'puppet:///modules/secure_linux_cis/audit_unlocked_system_accounts.sh',
-    mode   => 'u+x',
-  }
-  -> exec { 'Lock not root system accounts':
-    command => '/root/scripts/lock_unlocked_system_accounts.sh',
-    unless  => 'test -z `/root/scripts/audit_unlocked_system_accounts.sh`',
+
+  exec { 'Lock not root system accounts':
+    command => 'awk -F: '($1!="root" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"') {print $1}' /etc/passwd | xargs -I '{}' passwd -S '{}' | awk '($2!="L" && $2!="LK") {print $1}' | while read -r user; do usermod -L "$user"; done',
     path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
   }
+  
 }
