ba8d1fd2211b056257fb410c5488c212459109f2
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-01 18:57:56 +0800

    Update ensure_only_strong_mac_algorithms_are_used.pp

--- manifests/rules/ensure_only_strong_mac_algorithms_are_used.pp
+++ manifests/rules/ensure_only_strong_mac_algorithms_are_used.pp
@@ -3,15 +3,11 @@
 # @summary Ensure only strong MAC algorithms are used 
 #
 class secure_linux_cis::rules::ensure_only_strong_mac_algorithms_are_used {
-  include secure_linux_cis::sshd_service
-
-  $mac_algorithm_array = join($secure_linux_cis::approved_mac_algorithms,',')
 
   file_line { 'ssh mac algorithms':
     ensure => 'present',
     path   => '/etc/ssh/sshd_config',
-    line   => "MACs ${mac_algorithm_array}",
+    line   => "MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512,hmac-sha2-256",
     match  => '^#?MACs',
-    notify => Class['secure_linux_cis::sshd_service'],
   }
 }
