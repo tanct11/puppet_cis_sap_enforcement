d4c4ee9cf27b19ab286784193bdcd63d6b0ab778
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-01 19:31:15 +0800

    Update ensure_ssh_idle_timeout_interval_is_configured.pp

--- manifests/rules/ensure_ssh_idle_timeout_interval_is_configured.pp
+++ manifests/rules/ensure_ssh_idle_timeout_interval_is_configured.pp
@@ -3,23 +3,18 @@
 # @summary Ensure SSH Idle Timeout Interval is configured 
 #
 class secure_linux_cis::rules::ensure_ssh_idle_timeout_interval_is_configured {
-  include secure_linux_cis::sshd_service
 
-  if $secure_linux_cis::client_alive_interval > 300 or $secure_linux_cis::client_alive_interval < 1 {
-    fail('The Client Alive Interval has been manually set past the 1 - 300 threshold')
-  }
+  
   file_line { 'ssh alive interval':
     ensure => 'present',
     path   => '/etc/ssh/sshd_config',
-    line   => "ClientAliveInterval ${secure_linux_cis::client_alive_interval}",
+    line   => "ClientAliveInterval 90",
     match  => '^\s*ClientAliveInterval',
-    notify => Class['secure_linux_cis::sshd_service'],
   }
   file_line { 'ssh alive count max':
     ensure => 'present',
     path   => '/etc/ssh/sshd_config',
-    line   => "ClientAliveCountMax ${secure_linux_cis::client_alive_count_max}",
+    line   => "ClientAliveCountMax 3",
     match  => '^\s*ClientAliveCountMax',
-    notify => Class['secure_linux_cis::sshd_service'],
   }
 }
