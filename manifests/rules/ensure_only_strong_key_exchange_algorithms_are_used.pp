f7b6887d5a839bf261af3e313f342a9ff4a35ac1
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-01 18:57:00 +0800

    Update ensure_only_strong_key_exchange_algorithms_are_used.pp

--- manifests/rules/ensure_only_strong_key_exchange_algorithms_are_used.pp
+++ manifests/rules/ensure_only_strong_key_exchange_algorithms_are_used.pp
@@ -3,15 +3,11 @@
 # @summary Ensure only strong Key Exchange algorithms are used 
 #
 class secure_linux_cis::rules::ensure_only_strong_key_exchange_algorithms_are_used {
-  include secure_linux_cis::sshd_service
-
-  $kex_array = join($secure_linux_cis::approved_kex,',')
 
   file_line { 'ssh kex':
     ensure => 'present',
     path   => '/etc/ssh/sshd_config',
-    line   => "KexAlgorithms ${kex_array}",
+    line   => "KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group14-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256",
     match  => '^#?KexAlgorithms',
-    notify => Class['secure_linux_cis::sshd_service'],
   }
 }
