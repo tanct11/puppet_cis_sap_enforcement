0aa0664b3995800131735321efcc6239d937f833
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-03 21:44:16 +0800

    Update ensure_cron_daemon_is_enabled_and_running.pp

--- manifests/rules/ensure_cron_daemon_is_enabled_and_running.pp
+++ manifests/rules/ensure_cron_daemon_is_enabled_and_running.pp
@@ -3,7 +3,7 @@
 # @summary Ensure cron daemon is enabled and running 
 #
 class secure_linux_cis::rules::ensure_cron_daemon_is_enabled_and_running {
-  service { $secure_linux_cis::cron_service:
+  service { cron:
     ensure => running,
     enable => true,
   }
