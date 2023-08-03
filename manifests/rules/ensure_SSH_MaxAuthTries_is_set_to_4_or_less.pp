69a8e86fbeb8cbfbe4079961bb23854b5a5f7a32
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-03 22:51:20 +0800

    Create ensure_SSH_MaxAuthTries_is_set_to_4_or_less.pp

---
+++ manifests/rules/ensure_SSH_MaxAuthTries_is_set_to_4_or_less.pp
@@ -0,0 +1,8 @@
+class  ensure_SSH_MaxAuthTries_is_set_to_4_or_less {
+        file_line { 'ensure_SSH_MaxAuthTries_is_set_to_4_or_less' :
+                ensure => present,
+                path   => '/etc/ssh/sshd_config',
+                line   => 'maxauthtries 4',
+                match  => "^(maxauthtries=[0-9]$)",
+  }
+}
