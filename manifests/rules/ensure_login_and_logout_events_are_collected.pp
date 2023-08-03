b0a8c1493d8006009866a505938b037c504170f6
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-03 13:23:12 +0800

    Update ensure_login_and_logout_events_are_collected.pp

--- manifests/rules/ensure_login_and_logout_events_are_collected.pp
+++ manifests/rules/ensure_login_and_logout_events_are_collected.pp
@@ -3,43 +3,19 @@
 # @summary Ensure login and logout events are collected 
 #
 class secure_linux_cis::rules::ensure_login_and_logout_events_are_collected {
-  Class['secure_linux_cis::rules::ensure_login_and_logout_events_are_collected']
-  ~> Class['secure_linux_cis::reboot']
   file_line { 'audit.rules login/logout 1':
     ensure => present,
     path   => '/etc/audit/rules.d/audit.rules',
     line   => '-w /var/log/lastlog -p wa -k logins',
   }
-  case $facts['osfamily'] {
-    'RedHat': {
-      if $facts['os']['release']['major'] == '8' {
-        file_line { 'audit.rules login/logout 2':
-          ensure => present,
-          path   => '/etc/audit/rules.d/audit.rules',
-          line   => '-w /var/log/faillog -p wa -k logins',
-        }
-      } else {
-        file_line { 'audit.rules login/logout 2':
-          ensure => present,
-          path   => '/etc/audit/rules.d/audit.rules',
-          line   => '-w /var/run/faillock/ -p wa -k logins',
-        }
-      }
+  file_line { 'audit.rules login/logout 2':
+    ensure => present,
+    path   => '/etc/audit/rules.d/audit.rules',
+    line   => '-w /var/log/faillog -p wa -k logins',
+  }  
+  file_line { 'audit.rules login/logout 3':
+    ensure => present,
+    path   => '/etc/audit/rules.d/audit.rules',
+    line   => '-w /var/log/tallylog -p wa -k logins',
     }
-    'Debian': {
-      file_line { 'audit.rules login/logout 2':
-        ensure => present,
-        path   => '/etc/audit/rules.d/audit.rules',
-        line   => '-w /var/log/faillog -p wa -k logins',
-      }
-      file_line { 'audit.rules login/logout 3':
-        ensure => present,
-        path   => '/etc/audit/rules.d/audit.rules',
-        line   => '-w /var/log/tallylog -p wa -k logins',
-      }
-    }
-    default: {
-      warning ("Login / logon event checking is not supported on os family ${facts['osfamily']}.")
-    }
-  }
 }
