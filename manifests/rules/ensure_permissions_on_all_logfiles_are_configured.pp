d073c8461516263557119bec1dbe2778641fc961
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-03 21:40:24 +0800

    Update ensure_permissions_on_all_logfiles_are_configured.pp

--- manifests/rules/ensure_permissions_on_all_logfiles_are_configured.pp
+++ manifests/rules/ensure_permissions_on_all_logfiles_are_configured.pp
@@ -3,26 +3,9 @@
 # @summary Ensure permissions on all logfiles are configured 
 #
 class secure_linux_cis::rules::ensure_permissions_on_all_logfiles_are_configured (
-  Boolean $enforced = true,
-  Array[Stdlib::Unixpath] $exclude_logs = $secure_linux_cis::exclude_logs,
-) {
-  file { '/usr/share/cis_scripts/var_log_permissions.sh':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0700',
-    content => file('secure_linux_cis/var_log_permissions.sh'),
-  }
-
-  # Fix permissions on all files reported by this fact as wrong.
-  if $facts['var_log_permissions'] {
-    $logfiles = split($facts['var_log_permissions'],'\n')
-
-    # Remove any ignored logs from the list.
-    $_logfiles = $logfiles - $exclude_logs
-
-    file { $_logfiles:
-      mode     => 'g-wx,o-rwx',  #lint:ignore:no_symbolic_file_modes
-    }
+  exec { 'secure_linux_cis::rules::ensure_permissions_on_all_logfiles_are_configured':
+    commmand  => "find /var/log -type f -exec chmod g-wx,o-rwx "{}" + -o -type d -exec chmod g-wx,o-rwx "{}" +",
+    path      => ['/usr/bin', '/usr/sbin',],
+    logoutput => true,
   }
 }
