3168572b895692209393b4e24866c32eced4e848
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-03 16:36:55 +0800

    Update ensure_changes_to_system_administration_scope_sudoers_is_collected.pp

--- manifests/rules/ensure_changes_to_system_administration_scope_sudoers_is_collected.pp
+++ manifests/rules/ensure_changes_to_system_administration_scope_sudoers_is_collected.pp
@@ -3,17 +3,15 @@
 # @summary Ensure changes to system administration scope (sudoers) is collected 
 #
 class secure_linux_cis::rules::ensure_changes_to_system_administration_scope_sudoers_is_collected {
-  Class['secure_linux_cis::rules::ensure_changes_to_system_administration_scope_sudoers_is_collected']
-  ~> Class['secure_linux_cis::reboot']
   file_line { 'audit.rules sudoers 1':
     ensure => present,
-    path   => '/etc/audit/rules.d/audit.rules',
+    path   => '/etc/audit/rules.d/cis_hardening.rules',
     line   => '-w /etc/sudoers -p wa -k scope',
   }
 
   file_line { 'audit.rules sudoers 2':
     ensure => present,
-    path   => '/etc/audit/rules.d/audit.rules',
+    path   => '/etc/audit/rules.d/cis_hardening.rules',
     line   => '-w /etc/sudoers.d/ -p wa -k scope',
   }
 }
