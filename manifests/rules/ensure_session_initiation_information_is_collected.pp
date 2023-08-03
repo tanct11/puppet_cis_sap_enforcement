297ac57cf733c78c994055e64c5d92016d72be9c
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-03 13:32:55 +0800

    Update ensure_session_initiation_information_is_collected.pp

--- manifests/rules/ensure_session_initiation_information_is_collected.pp
+++ manifests/rules/ensure_session_initiation_information_is_collected.pp
@@ -3,21 +3,19 @@
 # @summary Ensure session initiation information is collected 
 #
 class secure_linux_cis::rules::ensure_session_initiation_information_is_collected {
-  Class['secure_linux_cis::rules::ensure_session_initiation_information_is_collected']
-  ~> Class['secure_linux_cis::reboot']
   file_line { 'audit.rules session 1':
     ensure => present,
-    path   => '/etc/audit/rules.d/audit.rules',
+    path   => '/etc/audit/rules.d/cis_hardening.rules',
     line   => '-w /var/run/utmp -p wa -k session',
   }
   file_line { 'audit.rules session 2':
     ensure => present,
-    path   => '/etc/audit/rules.d/audit.rules',
+    path   => '/etc/audit/rules.d/cis_hardening.rules',
     line   => '-w /var/log/wtmp -p wa -k logins',
   }
   file_line { 'audit.rules session 3':
     ensure => present,
-    path   => '/etc/audit/rules.d/audit.rules',
+    path   => '/etc/audit/rules.d/cis_hardening.rules',
     line   => '-w /var/log/btmp -p wa -k logins',
   }
 }
