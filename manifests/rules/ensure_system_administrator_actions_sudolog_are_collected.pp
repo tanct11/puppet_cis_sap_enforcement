0c0a0b14bdd1983e67a9afdde4d496c0598e0207
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-03 16:41:17 +0800

    Update ensure_system_administrator_actions_sudolog_are_collected.pp

--- manifests/rules/ensure_system_administrator_actions_sudolog_are_collected.pp
+++ manifests/rules/ensure_system_administrator_actions_sudolog_are_collected.pp
@@ -3,11 +3,9 @@
 # @summary Ensure system administrator actions (sudolog) are collected 
 #
 class secure_linux_cis::rules::ensure_system_administrator_actions_sudolog_are_collected {
-  Class['secure_linux_cis::rules::ensure_system_administrator_actions_sudolog_are_collected']
-  ~> Class['secure_linux_cis::reboot']
   file_line { 'audit.rules sudo.log 1':
     ensure => present,
-    path   => '/etc/audit/rules.d/audit.rules',
+    path   => '/etc/audit/rules.d/cis_hardening.rules',
     line   => '-w /var/log/sudo.log -p wa -k actions',
   }
 }
