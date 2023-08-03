c4ec849539d6770df6b38dcc1e679423e36e7c23
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-03 13:31:48 +0800

    Update ensure_login_and_logout_events_are_collected.pp

--- manifests/rules/ensure_login_and_logout_events_are_collected.pp
+++ manifests/rules/ensure_login_and_logout_events_are_collected.pp
@@ -5,17 +5,17 @@
 class secure_linux_cis::rules::ensure_login_and_logout_events_are_collected {
   file_line { 'audit.rules login/logout 1':
     ensure => present,
-    path   => '/etc/audit/rules.d/audit.rules',
+    path   => '/etc/audit/rules.d/cis_hardening.rules',
     line   => '-w /var/log/lastlog -p wa -k logins',
   }
   file_line { 'audit.rules login/logout 2':
     ensure => present,
-    path   => '/etc/audit/rules.d/audit.rules',
+    path   => '/etc/audit/rules.d/cis_hardening.rules',
     line   => '-w /var/log/faillog -p wa -k logins',
   }  
   file_line { 'audit.rules login/logout 3':
     ensure => present,
-    path   => '/etc/audit/rules.d/audit.rules',
+    path   => '/etc/audit/rules.d/cis_hardening.rules',
     line   => '-w /var/log/tallylog -p wa -k logins',
     }
 }
