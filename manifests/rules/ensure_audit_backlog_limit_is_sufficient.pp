8537e33d0ae08f3639ffdea232b005f41692e099
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-03 12:35:31 +0800

    Update ensure_audit_backlog_limit_is_sufficient.pp

--- manifests/rules/ensure_audit_backlog_limit_is_sufficient.pp
+++ manifests/rules/ensure_audit_backlog_limit_is_sufficient.pp
@@ -3,8 +3,10 @@
 # @summary Ensure audit_backlog_limit is sufficient 
 #
 class secure_linux_cis::rules::ensure_audit_backlog_limit_is_sufficient {
-  kernel_parameter { 'audit_backlog_limit=8192':
+   file_line { 'config_ssh_timeout':
     ensure   => present,
-    bootmode => 'default',
+    path     => '/boot/grub2/grub.cfg',
+    line     => 'audit_backlog_limit=32768',
+    match    => "^(audit_backlog_limit=[0-9]$)",
   }
 }
