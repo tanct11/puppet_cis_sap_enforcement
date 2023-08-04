cd8bb17b4ff5fa6d9fe3f921b4eaafa51bc19cfa
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-04 23:06:40 +0800

    Update ensure_accounts_in_etc_passwd_use_shadowed_passwords.pp

--- manifests/rules/ensure_accounts_in_etc_passwd_use_shadowed_passwords.pp
+++ manifests/rules/ensure_accounts_in_etc_passwd_use_shadowed_passwords.pp
@@ -3,5 +3,9 @@
 # @summary Ensure accounts in /etc/passwd use shadowed passwords 
 #
 class secure_linux_cis::rules::ensure_accounts_in_etc_passwd_use_shadowed_passwords {
-  # TODO: fix
-}
+  exec { 'ensure_accounts_in_etc_passwd_use_shadowed_passwords':
+    command   => "sed -e 's/^\([a-zA-Z0-9_]*\):[^:]*:/\1:x:/' -i /etc/passwd",
+    path      => ['/usr/bin', '/usr/sbin',],
+    logoutput => true,
+  }
+}}
