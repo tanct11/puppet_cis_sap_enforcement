759ed86c8ddb7171afa310c9026d6cb822b157f8
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-04 22:37:01 +0800

    Delete ensure_a_web_server_is_not_installed.pp

--- manifests/rules/ensure_a_web_server_is_not_installed.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure a web server is not installed 
-#
-class secure_linux_cis::rules::ensure_a_web_server_is_not_installed {
-  package { ['httpd', 'nginx']:
-    ensure => absent,
-  }
-}
