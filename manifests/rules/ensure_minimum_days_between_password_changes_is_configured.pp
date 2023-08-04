91ba54e44fa624ccbc95f3be3ebf1bef5aaa537e
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-04 12:36:47 +0800

    Create ensure_minimum_days_between_password_changes_is_configured.pp

---
+++ manifests/rules/ensure_minimum_days_between_password_changes_is_configured.pp
@@ -0,0 +1,7 @@
+# @api private
+#
+# @summary Ensure minimum days between password changes is configured 
+#
+class secure_linux_cis::rules::ensure_minimum_days_between_password_changes_is_configured {
+ 
+}
