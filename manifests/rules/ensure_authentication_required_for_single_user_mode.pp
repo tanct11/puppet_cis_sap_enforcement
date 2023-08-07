41f55ac865e27b94d5b8e17d9c774b77dd0f373d
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-07 15:15:33 +0800

    Update ensure_authentication_required_for_single_user_mode.pp

--- manifests/rules/ensure_authentication_required_for_single_user_mode.pp
+++ manifests/rules/ensure_authentication_required_for_single_user_mode.pp
@@ -3,12 +3,22 @@
 # @summary Ensure authentication required for single user mode 
 #
 class secure_linux_cis::rules::ensure_authentication_required_for_single_user_mode {
-  file { '/etc/systemd/system/rescue.service.d':
-    ensure  => directory,
-  }
+        file_line { 'rescue' :
+               ensure => present,
+               path   => '/usr/lib/systemd/system/rescue.service',
+               line   => 'ExecStart=-/usr/lib/systemd/systemd-sulogin-shell rescue',
+               match  => '(ExecStart=-.+)',
+               multiple => true,
+               replace_all_matches_not_matching_line => true,
+}
 
-  file { '/etc/systemd/system/rescue.service.d/00-require-auth.conf':
-    ensure  => file,
-    content => 'puppet:///modules/secure_linux_cis/00-require-auth.conf',
-  }
+        file_line { 'emergency' :
+               ensure => present,
+               path   => '/usr/lib/systemd/system/emergency.service',
+               line   => 'ExecStart=-/usr/lib/systemd/systemd-sulogin-shell emergency',
+               match  => '(ExecStart=-.+)',
+               multiple => true,
+               replace_all_matches_not_matching_line => true,
+    }
+
 }
