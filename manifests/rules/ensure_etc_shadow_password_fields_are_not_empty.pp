1f2e1c184a96b1ce943e261e812937b076a58b38
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-07 21:05:47 +0800

    Update ensure_etc_shadow_password_fields_are_not_empty.pp

--- manifests/rules/ensure_etc_shadow_password_fields_are_not_empty.pp
+++ manifests/rules/ensure_etc_shadow_password_fields_are_not_empty.pp
@@ -3,11 +3,12 @@
 # @summary Ensure /etc/shadow password fields are not empty   
 #
 class secure_linux_cis::rules::ensure_etc_shadow_password_fields_are_not_empty {
-  if($facts['no_password_accounts']) {
-    $facts['no_password_accounts'].each | String $account | {
-      accounts::user { $facts['no_password_accounts']:
-        locked => true,
-      }
-    }
-  }
+        exec {'test' :
+                command   => 'var=$(awk -F: '($2 == "" ) { print $1 " does not have a password "}' /etc/shadow);
+                              passwd -l $(var)',
+                logoutput => true,
+                path      => ['/usr/bin', '/usr/sbin',],
+                provider  => 'shell',
+
+}
 }
