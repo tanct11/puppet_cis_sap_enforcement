7e202a90b8289e62f6df952804673504be822b8b
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-07 14:57:11 +0800

    Delete ensure_auditd_service_is_enabled_and_active.pp

--- manifests/rules/ensure_auditd_service_is_enabled_and_active.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure auditd service is enabled and active 
-#
-class secure_linux_cis::rules::ensure_auditd_service_is_enabled_and_active {
-  include secure_linux_cis::rules::ensure_auditd_service_is_enabled_and_running
-}
