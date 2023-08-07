7157fee43e98bcbbee30e75dca3f675c202cd8be
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-07 17:21:46 +0800

    Update ensure_chrony_is_configured.pp

--- manifests/rules/ensure_chrony_is_configured.pp
+++ manifests/rules/ensure_chrony_is_configured.pp
@@ -3,29 +3,24 @@
 # @summary Ensure chrony is configured 
 #
 class secure_linux_cis::rules::ensure_chrony_is_configured {
-  case $facts['osfamily'] {
-    'RedHat': {
-      $config = '/etc/sysconfig/chronyd'
-      $content = 'OPTIONS="-u chrony"'
-    }
-    'Debian': {
-      $config = '/etc/default/chrony'
-      $content = 'DAEMON_OPTS="-u _chrony"'
-    }
-    default: {
-      warning ("Chrony check is not supported on os family ${facts['osfamily']}.")
-    }
-  }
-  if $secure_linux_cis::time_sync == 'chrony' {
-    class { 'chrony':
-      servers  => $secure_linux_cis::time_servers,
-    }
-    file { $config:
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => $content,
-    }
-  }
+  file_line {'chrony_is_configured 1' :
+   ensure => 'absent',
+   path   => '/home/ccpro/test2',
+   match  => "^(server)",
+   match_for_absence => true,
+   multiple => true,
+
+}
+  file_line {'chrony_is_configured 2' :
+    ensure => 'present',
+    path   => '/home/ccpro/test2',
+    line   => 'server 192.168.16.1 iburst',
+}
+ file_line {'chrony_is_configured 3' :
+    ensure => 'present',
+    path   => '/home/ccpro/test2',
+    line   => 'server 192.168.128.1 iburst',
+
+}
+
 }
