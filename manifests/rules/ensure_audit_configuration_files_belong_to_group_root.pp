7876e6bb4fddd880049815bc0150efb94d410d4b
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-07 13:36:51 +0800

    Delete ensure_audit_configuration_files_belong_to_group_root.pp

--- manifests/rules/ensure_audit_configuration_files_belong_to_group_root.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure audit configuration files belong to group root 
-#
-class secure_linux_cis::rules::ensure_audit_configuration_files_belong_to_group_root {
-  include secure_linux_cis::rules::ensure_audit_configuration_files_are_owned_by_root
-}
