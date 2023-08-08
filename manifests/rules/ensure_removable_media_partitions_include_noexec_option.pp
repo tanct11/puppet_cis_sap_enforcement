769289aed4fd4388fb2e9072362ba4b91824a2b1
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 21:07:28 +0800

    Delete ensure_removable_media_partitions_include_noexec_option.pp

--- manifests/rules/ensure_removable_media_partitions_include_noexec_option.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure removable media partitions include noexec option 
-#
-class secure_linux_cis::rules::ensure_removable_media_partitions_include_noexec_option {
-  include secure_linux_cis::rules::ensure_noexec_option_set_on_removable_media_partitions
-}
