e06232ca5f9724911e505837a20a5a28088db7d4
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 15:12:13 +0800

    Update ensure_gpgcheck_is_globally_activated.pp

--- manifests/rules/ensure_gpgcheck_is_globally_activated.pp
+++ manifests/rules/ensure_gpgcheck_is_globally_activated.pp
@@ -3,12 +3,11 @@
 # @summary Ensure gpgcheck is globally activated 
 #
 class secure_linux_cis::rules::ensure_gpgcheck_is_globally_activated (
-  Stdlib::Absolutepath $package_configuration,
 ) {
   file_line { 'gpgcheck':
     ensure => present,
     after  => '[main]',
-    path   => $package_configuration,
+    path   => '$/etc/zypp/zypp.conf',
     line   => 'gpgcheck=1',
     match  => '^gpgcheck',
   }
