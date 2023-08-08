2391686e39ac399d2d54a6205362ab8c5b14b2b3
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 15:06:16 +0800

    Delete ensure_gnome_display_manager_is_removed.pp

--- manifests/rules/ensure_gnome_display_manager_is_removed.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure GNOME Display Manager is removed 
-#
-class secure_linux_cis::rules::ensure_gnome_display_manager_is_removed {
-  package { 'gdm3':
-    ensure => absent,
-  }
-}
