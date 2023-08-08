b9f6f9621989d49880c2fd310192f3ce28459525
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 20:32:41 +0800

    Delete ensure_permissions_on_etc_hosts_allow_are_configured.pp

--- manifests/rules/ensure_permissions_on_etc_hosts_allow_are_configured.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure permissions on /etc/hosts.allow are configured 
-#
-class secure_linux_cis::rules::ensure_permissions_on_etc_hosts_allow_are_configured {
-  # This benchmark is managed in secure_linux_cis::ensure_permissions_on_etc_hosts_allow_are_configured
-}
