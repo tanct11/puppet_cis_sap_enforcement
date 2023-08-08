440086a14af8e3c2eb9077396f53a270fa50366b
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 22:21:33 +0800

    Delete ensure_snmp_server_is_not_installed.pp

--- manifests/rules/ensure_snmp_server_is_not_installed.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure SNMP Server is not installed 
-#
-class secure_linux_cis::rules::ensure_snmp_server_is_not_installed {
-  package { 'snmpd':
-    ensure => absent,
-  }
-}
