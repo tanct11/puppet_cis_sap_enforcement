168b1402c5b6b8467dd1f151e310da886361601c
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 20:57:03 +0800

    Update ensure_prelink_is_disabled.pp

--- manifests/rules/ensure_prelink_is_disabled.pp
+++ manifests/rules/ensure_prelink_is_disabled.pp
@@ -3,16 +3,12 @@
 # @summary Ensure prelink is disabled 
 #
 class secure_linux_cis::rules::ensure_prelink_is_disabled {
-  case $facts['osfamily'] {
-    'Suse': {
       package { 'prelink':
         ensure   => absent,
-      }
-    }
-    default: {
-      package { 'prelink':
-        ensure   => absent,
-      }
-    }
   }
+    exec { 'prelink 2':
+      command   => 'prelink -ua',
+      path      => ['/usr/bin', '/usr/sbin',],
+      logoutput => true,
+      
 }
