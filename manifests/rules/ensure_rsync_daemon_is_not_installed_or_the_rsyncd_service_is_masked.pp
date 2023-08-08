fc2d13058715e16012d8fa9b24ec42e31dd63b79
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 21:36:38 +0800

    Delete ensure_rsync_daemon_is_not_installed_or_the_rsyncd_service_is_masked.pp

--- manifests/rules/ensure_rsync_daemon_is_not_installed_or_the_rsyncd_service_is_masked.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure rsync-daemon is not installed or the rsyncd service is masked 
-#
-class secure_linux_cis::rules::ensure_rsync_daemon_is_not_installed_or_the_rsyncd_service_is_masked {
-  package { 'rsync':
-    ensure => absent,
-  }
-}
