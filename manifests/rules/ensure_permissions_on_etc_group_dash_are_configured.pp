455149477118734b3c95d98b87a2b28badf06019
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 20:31:07 +0800

    Update ensure_permissions_on_etc_group_dash_are_configured.pp

--- manifests/rules/ensure_permissions_on_etc_group_dash_are_configured.pp
+++ manifests/rules/ensure_permissions_on_etc_group_dash_are_configured.pp
@@ -7,16 +7,12 @@
 # @summary  Ensure permissions on /etc/group- are configured (Scored)
 #
 class secure_linux_cis::rules::ensure_permissions_on_etc_group_dash_are_configured {
-  $os = "${facts['os']['name']}${facts['os']['release']['major']}"
-  $mode = $os ? {
-    'Debian10' => '0600',
-    default    => '0644',
-  }
+
 
   file { '/etc/group-':
     ensure => file,
     owner  => 'root',
     group  => 'root',
-    mode   => $mode,
+    mode   => '0644',
   }
 }
