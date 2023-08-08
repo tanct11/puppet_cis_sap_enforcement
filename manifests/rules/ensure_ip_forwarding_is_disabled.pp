43283d1c2a79122e7e8e6ad873edb2876aaecdd7
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 19:50:50 +0800

    Update ensure_ip_forwarding_is_disabled.pp

--- manifests/rules/ensure_ip_forwarding_is_disabled.pp
+++ manifests/rules/ensure_ip_forwarding_is_disabled.pp
@@ -6,9 +6,5 @@ class secure_linux_cis::rules::ensure_ip_forwarding_is_disabled {
   sysctl { 'net.ipv4.ip_forward':
     value    => 0,
   }
-  if $facts['osfamily'] == 'Debian' and $secure_linux_cis::ipv6_enabled {
-    sysctl { 'net.ipv6.conf.all.forwarding':
-      value    => 0,
-    }
-  }
+
 }
