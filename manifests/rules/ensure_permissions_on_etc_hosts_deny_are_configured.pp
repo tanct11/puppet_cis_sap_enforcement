e41c45ad42fbc7eb71a6f04d49fd32198f67e6be
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 20:33:13 +0800

    Delete ensure_permissions_on_etc_hosts_deny_are_configured.pp

--- manifests/rules/ensure_permissions_on_etc_hosts_deny_are_configured.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure permissions on /etc/hosts.deny are configured 
-#
-class secure_linux_cis::rules::ensure_permissions_on_etc_hosts_deny_are_configured {
-  # This benchmark is managed in secure_linux_cis::ensure_permissions_on_etc_hosts_deny_are_configured
-}
