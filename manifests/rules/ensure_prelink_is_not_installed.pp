242cf307e31ee0969c0a6c0cc029062f1f757085
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 20:57:25 +0800

    Delete ensure_prelink_is_not_installed.pp

--- manifests/rules/ensure_prelink_is_not_installed.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure prelink is not installed 
-#
-class secure_linux_cis::rules::ensure_prelink_is_not_installed {
-  include secure_linux_cis::rules::ensure_prelink_is_disabled
-}
