5a4d9b330317e01a31898a1a106af957141850ff
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 20:41:10 +0800

    Update ensure_permissions_on_etc_passwd_dash_are_configured.pp

--- manifests/rules/ensure_permissions_on_etc_passwd_dash_are_configured.pp
+++ manifests/rules/ensure_permissions_on_etc_passwd_dash_are_configured.pp
@@ -3,11 +3,6 @@
 # @summary Ensure permissions on /etc/group- are configured 
 #
 class secure_linux_cis::rules::ensure_permissions_on_etc_passwd_dash_are_configured {
-  $os = "${facts['os']['name']}${facts['os']['release']['major']}"
-  $mode = $os ? {
-    'Debian10' => '0600',
-    default    => '0644',
-  }
 
   file { '/etc/passwd-':
     ensure => file,
