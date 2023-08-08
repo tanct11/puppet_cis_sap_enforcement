a718643d5bf58d813cc96818e97c778e68d90925
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 21:45:31 +0800

    Delete ensure_selinux_is_installed.pp

--- manifests/rules/ensure_selinux_is_installed.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure SELinux is installed 
-#
-class secure_linux_cis::rules::ensure_selinux_is_installed {
-  package { 'libselinux':
-    ensure   => present,
-  }
-}
