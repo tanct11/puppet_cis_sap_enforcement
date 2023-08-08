7fe13bd47ea49b65cded25f2de2c31d27512f289
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 14:58:15 +0800

    Update ensure_firewalld_service_is_enabled_and_running.pp

--- manifests/rules/ensure_firewalld_service_is_enabled_and_running.pp
+++ manifests/rules/ensure_firewalld_service_is_enabled_and_running.pp
@@ -7,4 +7,17 @@ class secure_linux_cis::rules::ensure_firewalld_service_is_enabled_and_running {
     ensure => running,
     enable => true,
   }
+
+  exec { 'firewalld_service_enabled_and_running 1' :
+    commmand => 'systemctl unmask firewalld',
+    path      => ['/usr/bin', '/usr/sbin',],
+    logoutput => true,
+}
+
+
+ exec { 'firewalld_service_enabled_and_running 2' :
+    commmand => 'systemctl --now enable firewalld',
+    path      => ['/usr/bin', '/usr/sbin',],
+    logoutput => true,
+}
 }
