164fe764a9a348d4ad4679fc4f583fae1be11df1
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 15:30:01 +0800

    Delete ensure_iptables_is_installed.pp

--- manifests/rules/ensure_iptables_is_installed.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure iptables is installed 
-#
-class secure_linux_cis::rules::ensure_iptables_is_installed {
-  package { 'iptables':
-    ensure => installed,
-  }
-}
