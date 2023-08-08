83d61974cdb571601948d22243580c2fc5f81f98
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 15:49:09 +0800

    Delete ensure_iptables_services_not_installed_with_firewalld.pp

--- manifests/rules/ensure_iptables_services_not_installed_with_firewalld.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure iptables-services not installed with firewalld   
-#
-class secure_linux_cis::rules::ensure_iptables_services_not_installed_with_firewalld {
-  package { 'firewalld':
-    ensure => absent,
-  }
-}
