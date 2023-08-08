a784cba983255574a141d6cbd466d3aa8b557e4e
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 15:46:31 +0800

    Delete ensure_iptables_persistent_is_not_installed_with_ufw.pp

--- manifests/rules/ensure_iptables_persistent_is_not_installed_with_ufw.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure iptables-persistent is not installed with ufw 
-#
-class secure_linux_cis::rules::ensure_iptables_persistent_is_not_installed_with_ufw {
-  package { 'iptables-persistent':
-    ensure => absent,
-  }
-}
