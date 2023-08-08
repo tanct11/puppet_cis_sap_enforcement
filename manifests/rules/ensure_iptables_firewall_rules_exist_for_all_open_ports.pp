3ac003ddaeb2cc4ed77431c9cef22d6e2f059126
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 15:24:06 +0800

    Delete ensure_iptables_firewall_rules_exist_for_all_open_ports.pp

--- manifests/rules/ensure_iptables_firewall_rules_exist_for_all_open_ports.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure iptables firewall rules exist for all open ports   
-#
-class secure_linux_cis::rules::ensure_iptables_firewall_rules_exist_for_all_open_ports {
-  # TODO
-}
