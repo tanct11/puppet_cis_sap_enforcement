29378205d6030c6abc8cee6e40698e1191a8f45a
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 14:45:21 +0800

    Update ensure_default_user_umask_is_configured.pp

--- manifests/rules/ensure_default_user_umask_is_configured.pp
+++ manifests/rules/ensure_default_user_umask_is_configured.pp
@@ -5,9 +5,12 @@
 class secure_linux_cis::rules::ensure_default_user_umask_is_configured {
 
   file_line { 'login.defs':
-    path  => '/etc/login.defs',
-    line  => 'UMASK 27',
-    match => '^\s*umask\s+\d+',
-    multiple => true,
+    path                                  => '/etc/login.defs',
+    line                                  => 'UMASK 27',
+    match                                 => '/^UMASK.+$/gi',
+    multiple                              => true,
+    replace_all_matches_not_matching_line => true,
   }
+
+
 }
