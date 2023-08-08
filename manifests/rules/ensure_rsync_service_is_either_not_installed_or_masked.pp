e494382f689c41dddd6b89317feefe935d46e242
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 21:37:39 +0800

    Delete ensure_rsync_service_is_either_not_installed_or_masked.pp

--- manifests/rules/ensure_rsync_service_is_either_not_installed_or_masked.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure rsync service is either not installed or masked 
-#
-class secure_linux_cis::rules::ensure_rsync_service_is_either_not_installed_or_masked {
-  package { 'rsync':
-    ensure => absent,
-  }
-}
