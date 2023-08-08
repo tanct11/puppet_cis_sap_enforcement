c2969cf5038a5a668e16b657a7b4fa500c662440
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 15:03:17 +0800

    Delete ensure_gdm_disabling_automatic_mounting_of_removable_media_is_not_overridden.pp

--- manifests/rules/ensure_gdm_disabling_automatic_mounting_of_removable_media_is_not_overridden.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure GDM disabling automatic mounting of removable media is not overridden 
-#
-class secure_linux_cis::rules::ensure_gdm_disabling_automatic_mounting_of_removable_media_is_not_overridden {
-  # TODO
-}
