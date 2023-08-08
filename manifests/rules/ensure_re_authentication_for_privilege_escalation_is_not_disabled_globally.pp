e4a7b73dd450ad3d56b89c1a09a9afebb0f877f7
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 20:59:10 +0800

    Delete ensure_re_authentication_for_privilege_escalation_is_not_disabled_globally.pp

--- manifests/rules/ensure_re_authentication_for_privilege_escalation_is_not_disabled_globally.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure re-authentication for privilege escalation is not disabled globally 
-#
-class secure_linux_cis::rules::ensure_re_authentication_for_privilege_escalation_is_not_disabled_globally {
-  # TODO
-}
