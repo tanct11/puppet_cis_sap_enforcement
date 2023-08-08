83b070cf47030e2fb510ecb09cb540a0c26acae1
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 15:00:17 +0800

    Delete ensure_ftp_client_is_not_installed.pp

--- manifests/rules/ensure_ftp_client_is_not_installed.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure FTP client is not installed 
-#
-class secure_linux_cis::rules::ensure_ftp_client_is_not_installed {
-  include secure_linux_cis::rules::ensure_ftp_server_is_not_installed
-}
