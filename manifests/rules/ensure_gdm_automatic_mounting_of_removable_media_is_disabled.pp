7520354a7948d6dac4fbbec9ea20e1883d613e3f
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 15:01:46 +0800

    Delete ensure_gdm_automatic_mounting_of_removable_media_is_disabled.pp

--- manifests/rules/ensure_gdm_automatic_mounting_of_removable_media_is_disabled.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure GDM automatic mounting of removable media is disabled 
-#
-class secure_linux_cis::rules::ensure_gdm_automatic_mounting_of_removable_media_is_disabled {
-  # TODO
-}
