f9397a865ef9b6e5a44efa751d4c95b541cf1fdb
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 21:38:44 +0800

    Delete ensure_rsync_service_is_not_installed.pp

--- manifests/rules/ensure_rsync_service_is_not_installed.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure rsync service is not installed 
-#
-class secure_linux_cis::rules::ensure_rsync_service_is_not_installed {
-  package { 'rsync':
-    ensure => absent,
-  }
-}
