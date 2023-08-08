97c859ec6082a27cf47a2ee32379a22e2a0f5bcf
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 21:32:32 +0800

    Delete ensure_rpcbind_is_not_installed_or_the_rpcbind_service_is_disabled.pp

--- manifests/rules/ensure_rpcbind_is_not_installed_or_the_rpcbind_service_is_disabled.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure rpcbind is not installed or the rpcbind service is disabled 
-#
-class secure_linux_cis::rules::ensure_rpcbind_is_not_installed_or_the_rpcbind_service_is_disabled {
-  package { 'rpcbind':
-    ensure => absent,
-  }
-}
