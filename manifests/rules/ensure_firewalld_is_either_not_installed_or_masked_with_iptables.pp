995cc9b5e8319d355e2ab8920ccfe733f7d28187
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 14:50:12 +0800

    Delete ensure_firewalld_is_either_not_installed_or_masked_with_iptables.pp

--- manifests/rules/ensure_firewalld_is_either_not_installed_or_masked_with_iptables.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure firewalld is either not installed or masked with iptables 
-#
-class secure_linux_cis::rules::ensure_firewalld_is_either_not_installed_or_masked_with_iptables {
-  package { 'firewalld':
-    ensure => absent,
-  }
-}
