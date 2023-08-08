4a809b137d5accc210f32b8912608754af012d4e
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 15:02:39 +0800

    Delete ensure_gdm_disable_user_list_option_is_enabled.pp

--- manifests/rules/ensure_gdm_disable_user_list_option_is_enabled.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure GDM disable-user-list option is enabled 
-#
-class secure_linux_cis::rules::ensure_gdm_disable_user_list_option_is_enabled {
-  package { 'gdm':
-    ensure => absent,
-  }
-}
