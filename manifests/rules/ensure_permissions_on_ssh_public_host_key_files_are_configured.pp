7d9c055ac2dc5bb5c78268b405c01f33481cc068
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 20:51:54 +0800

    Update ensure_permissions_on_ssh_public_host_key_files_are_configured.pp

--- manifests/rules/ensure_permissions_on_ssh_public_host_key_files_are_configured.pp
+++ manifests/rules/ensure_permissions_on_ssh_public_host_key_files_are_configured.pp
@@ -3,12 +3,10 @@
 # @summary Ensure permissions on SSH public host key files are configured 
 #
 class secure_linux_cis::rules::ensure_permissions_on_ssh_public_host_key_files_are_configured {
-  include secure_linux_cis::sshd_service
-
-  file { $facts['ssh_host_pub_keys']:
-    ensure => 'file',
-    owner  => 'root',
-    group  => 'root',
-    mode   => 'u-x,go-wx',
-  }
+    exec { 'public'
+      command => "find /etc/ssh -xdev -type f -name 'ssh_host_*_key.pub' -exec chmod u-x,go-wx {} \;
+                  find /etc/ssh -xdev -type f -name 'ssh_host_*_key.pub' -exec chown root:root {} \;",
+      path      => ['/usr/bin', '/usr/sbin',],
+      logoutput => true,
+    }
 }
