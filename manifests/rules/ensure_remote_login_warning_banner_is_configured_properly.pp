1b28fa7720f6de5935dead193552394f8321990a
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-08 21:05:05 +0800

    Update ensure_remote_login_warning_banner_is_configured_properly.pp

--- manifests/rules/ensure_remote_login_warning_banner_is_configured_properly.pp
+++ manifests/rules/ensure_remote_login_warning_banner_is_configured_properly.pp
@@ -3,10 +3,23 @@
 # @summary Ensure remote login warning banner is configured properly 
 #
 class secure_linux_cis::rules::ensure_remote_login_warning_banner_is_configured_properly {
-  if $facts['issue_net'] {
-    notify { 'issuen':
-      message  => 'Not in compliance with CIS (Scored). OS and/or patch level information in /etc/issue.net',
-      loglevel => 'warning',
+ file { 'login_warning_banner' :
+		ensure => 'present',
+		path   => '/etc/issue.net',
+		content => "PROPRIETARY SYSTEM FOR P&G BUSINESS USE ONLY
+
+              This is a private computer/communication system. Access to it for any reason must be specifically authorized. If you are
+              not authorized, your continued access and further inquiry may expose you to criminal and/or civil proceedings. System
+              personnel will monitor this system for unauthorized activity, misuse, and to evaluate system performance. Anyone using
+              this system expressly consents to such monitoring.
+
+               Software must not be installed on this computer/communication system without a purchased and documented license
+               approved by the Company's licensing process. Failure to comply could expose you and/or the Company to legal
+               proceedings.
+
+               Consistent with and to the extent permitted by law, P&G will monitor user activity on P&G systems for security purposes.
+               P&G employees should consult the P&G Employee Privacy Policy for more information. If you are non-employee, please
+               contact your employer about an alternative arrangement should you not wish to participate in P&G's security monitoring
+               program.",
     }
-  }
 }
