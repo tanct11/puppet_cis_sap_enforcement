d92e18dc051a531ea3fcb143295141cff426af11
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 11:54:11 +0800

    Update ensure_ssh_hostbasedauthentication_is_disabled.pp

--- manifests/rules/ensure_ssh_hostbasedauthentication_is_disabled.pp
+++ manifests/rules/ensure_ssh_hostbasedauthentication_is_disabled.pp
@@ -3,13 +3,11 @@
 # @summary Ensure SSH HostbasedAuthentication is disabled 
 #
 class secure_linux_cis::rules::ensure_ssh_hostbasedauthentication_is_disabled {
-  include secure_linux_cis::sshd_service
 
   file_line { 'ssh host based authentication':
     ensure => 'present',
     path   => '/etc/ssh/sshd_config',
     line   => 'HostbasedAuthentication no',
     match  => '^HostbasedAuthentication',
-    notify => Class['secure_linux_cis::sshd_service'],
   }
 }
