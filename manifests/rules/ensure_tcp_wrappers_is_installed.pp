4589795e136d3ac072dec9509e4d97e63b104e6b
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 13:35:24 +0800

    Delete ensure_tcp_wrappers_is_installed.pp

--- manifests/rules/ensure_tcp_wrappers_is_installed.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure TCP Wrappers is installed (deprecated)
-#
-class secure_linux_cis::rules::ensure_tcp_wrappers_is_installed {
-  # Handle TCP traffic through your firewall
-}
