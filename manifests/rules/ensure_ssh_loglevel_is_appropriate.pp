daea99a10ff54bac79808315fb8b19887ad363de
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 11:56:27 +0800

    Update ensure_ssh_loglevel_is_appropriate.pp

--- manifests/rules/ensure_ssh_loglevel_is_appropriate.pp
+++ manifests/rules/ensure_ssh_loglevel_is_appropriate.pp
@@ -3,6 +3,10 @@
 # @summary Ensure SSH LogLevel is appropriate 
 #
 class secure_linux_cis::rules::ensure_ssh_loglevel_is_appropriate {
-  # Both VERBOSE and INFO are considered appropriate, for Debian8 and Debian9
-  include secure_linux_cis::rules::ensure_ssh_loglevel_is_set_to_info
+  file_line { 'ssh log level':
+    ensure => present,
+    path   => '/etc/ssh/sshd_config',
+    line   => 'LogLevel INFO',
+    match  => '^LogLevel',
+  }
 }
