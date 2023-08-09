59d4b9f79d2f70880642e5d2b5e3e9eebec51262
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 14:42:13 +0800

    Delete ensure_var_tmp_partition_includes_the_nosuid_option.pp

--- manifests/rules/ensure_var_tmp_partition_includes_the_nosuid_option.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure /var/tmp partition includes the nosuid option 
-#
-class secure_linux_cis::rules::ensure_var_tmp_partition_includes_the_nosuid_option {
-  include secure_linux_cis::rules::ensure_var_tmp_partition_includes_the_nosuid_option
-}
