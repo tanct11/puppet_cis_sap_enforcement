d7e9e5c721feb302e11f040498464c06379ab0c2
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 14:14:12 +0800

    Delete ensure_ufw_default_deny_firewall_policy.pp

--- manifests/rules/ensure_ufw_default_deny_firewall_policy.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure ufw default deny firewall policy 
-#
-class secure_linux_cis::rules::ensure_ufw_default_deny_firewall_policy {
-  include secure_linux_cis::rules::ensure_ufw_is_installed
-}
