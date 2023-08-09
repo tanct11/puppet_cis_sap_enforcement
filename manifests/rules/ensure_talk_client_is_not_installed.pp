6cbd520c33d9baec3230662f9635d35e8f3e6b81
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 13:32:40 +0800

    Update ensure_talk_client_is_not_installed.pp

--- manifests/rules/ensure_talk_client_is_not_installed.pp
+++ manifests/rules/ensure_talk_client_is_not_installed.pp
@@ -2,17 +2,8 @@
 #
 # @summary Ensure talk client is not installed 
 #
-class secure_linux_cis::rules::ensure_talk_client_is_not_installed {
-  case $facts['osfamily'] {
-    'Suse': {
+class secure_linux_cis::rules::ensure_talk_client_is_not_installed { 
       package { 'talk':
         ensure   => absent,
       }
-    }
-    default: {
-      package { 'talk':
-        ensure   => absent,
-      }
-    }
-  }
 }
