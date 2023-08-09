f924f5c8330e3df319b34c2ee0747326e1996b20
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 16:24:51 +0800

    Update ensure_sticky_bit_is_set_on_all_world_writable_directories.pp

--- manifests/rules/ensure_sticky_bit_is_set_on_all_world_writable_directories.pp
+++ manifests/rules/ensure_sticky_bit_is_set_on_all_world_writable_directories.pp
@@ -5,6 +5,7 @@
 class secure_linux_cis::rules::ensure_sticky_bit_is_set_on_all_world_writable_directories {
     exec { '1_1_21 - Ensure sticky bit is set on all world-writable directories (Scored)':
       command   => "df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null | xargs -I '{}' chmod a+t '{}'",  #lint:ignore:140chars
+       path      => ['/usr/bin', '/usr/sbin',],
       logoutput => true,
     }
   }
