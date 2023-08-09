baf027f8e95a176f466643fa170c68de8c5ba7a7
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 11:53:49 +0800

    Update ensure_ssh_allowtcpforwarding_is_disabled.pp

--- manifests/rules/ensure_ssh_allowtcpforwarding_is_disabled.pp
+++ manifests/rules/ensure_ssh_allowtcpforwarding_is_disabled.pp
@@ -3,13 +3,11 @@
 # @summary Ensure SSH AllowTcpForwarding is disabled 
 #
 class secure_linux_cis::rules::ensure_ssh_allowtcpforwarding_is_disabled {
-  include secure_linux_cis::sshd_service
 
   file_line { 'ssh tcpforward':
     ensure => present,
     path   => '/etc/ssh/sshd_config',
     line   => 'AllowTcpForwarding no',
     match  => '^#?[\r\n\f\v ]?AllowTcpForwarding',
-    notify => Class['secure_linux_cis::sshd_service'],
   }
 }
