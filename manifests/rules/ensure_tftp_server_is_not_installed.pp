d4aaa58e092dc2e379712ccf1c76f812dac6dadd
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 13:37:10 +0800

    Delete ensure_tftp_server_is_not_installed.pp

--- manifests/rules/ensure_tftp_server_is_not_installed.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure TFTP Server is not installed 
-#
-class secure_linux_cis::rules::ensure_tftp_server_is_not_installed {
-  package { 'tftp-server':
-    ensure => absent,
-  }
-}
