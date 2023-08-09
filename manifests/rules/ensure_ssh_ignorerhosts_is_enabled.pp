d17fac544aa480d27cc2785460452a8c2f4a74a5
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 11:55:05 +0800

    Update ensure_ssh_ignorerhosts_is_enabled.pp

--- manifests/rules/ensure_ssh_ignorerhosts_is_enabled.pp
+++ manifests/rules/ensure_ssh_ignorerhosts_is_enabled.pp
@@ -3,13 +3,11 @@
 # @summary Ensure SSH IgnoreRhosts is enabled 
 #
 class secure_linux_cis::rules::ensure_ssh_ignorerhosts_is_enabled {
-  include secure_linux_cis::sshd_service
 
   file_line { 'ssh ignore rhosts':
     ensure => present,
     path   => '/etc/ssh/sshd_config',
     line   => 'IgnoreRhosts yes',
     match  => '^#?IgnoreRhosts',
-    notify => Class['secure_linux_cis::sshd_service'],
   }
 }
