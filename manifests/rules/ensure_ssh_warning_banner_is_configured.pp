d598e83995dac8ef951752fa178e9caa6936f59f
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 13:11:31 +0800

    Update ensure_ssh_warning_banner_is_configured.pp

--- manifests/rules/ensure_ssh_warning_banner_is_configured.pp
+++ manifests/rules/ensure_ssh_warning_banner_is_configured.pp
@@ -3,13 +3,11 @@
 # @summary Ensure SSH warning banner is configured 
 #
 class secure_linux_cis::rules::ensure_ssh_warning_banner_is_configured {
-  include secure_linux_cis::sshd_service
 
   file_line { 'ssh warning banner':
     ensure => 'present',
     path   => '/etc/ssh/sshd_config',
     line   => 'Banner /etc/issue.net',
     match  => '^#?Banner',
-    notify => Class['secure_linux_cis::sshd_service'],
   }
 }
