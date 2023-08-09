d72049e55572dd656627979eb90721e0fb3b020b
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 11:57:33 +0800

    Update ensure_ssh_maxstartups_is_configured.pp

--- manifests/rules/ensure_ssh_maxstartups_is_configured.pp
+++ manifests/rules/ensure_ssh_maxstartups_is_configured.pp
@@ -3,13 +3,11 @@
 # @summary Ensure SSH MaxStartups is configured 
 #
 class secure_linux_cis::rules::ensure_ssh_maxstartups_is_configured {
-  include secure_linux_cis::sshd_service
 
   file_line { 'ssh max startups':
     ensure => present,
     path   => '/etc/ssh/sshd_config',
     line   => "MaxStartups 10:30:60",
     match  => '^\s*MaxStartups',
-    notify => Class['secure_linux_cis::sshd_service'],
   }
 }
