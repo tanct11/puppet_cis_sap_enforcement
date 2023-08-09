23bd235ec4c036e9efba48be889b7d4797c4c0a9
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 14:42:45 +0800

    Delete ensure_var_tmp_partition_includes_the_noexec_option.pp

--- manifests/rules/ensure_var_tmp_partition_includes_the_noexec_option.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure /var/tmp partition includes the noexec option 
-#
-class secure_linux_cis::rules::ensure_var_tmp_partition_includes_the_noexec_option {
-  include secure_linux_cis::rules::ensure_noexec_option_set_on_var_tmp_partition
-}
