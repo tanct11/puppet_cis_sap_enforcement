c26ed95aa115ce6fa9bfcc456acac6696d1eed99
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 13:36:50 +0800

    Delete ensure_tftp_client_is_not_installed.pp

--- manifests/rules/ensure_tftp_client_is_not_installed.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure TFTP client is not installed 
-#
-class secure_linux_cis::rules::ensure_tftp_client_is_not_installed {
-  package { 'tftp':
-    ensure => absent,
-  }
-}
