e0092dbfe1b57bfae7b21d68967a073dc2b6f431
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 14:19:44 +0800

    Update ensure_updates_patches_and_additional_security_software_are_installed.pp

--- manifests/rules/ensure_updates_patches_and_additional_security_software_are_installed.pp
+++ manifests/rules/ensure_updates_patches_and_additional_security_software_are_installed.pp
@@ -3,9 +3,8 @@
 # @summary Ensure updates, patches, and additional security software are installed 
 #
 class secure_linux_cis::rules::ensure_updates_patches_and_additional_security_software_are_installed {
-  cron::monthly { 'security-update':
-    ensure      => present,
-    environment => ['MAILTO=root', 'PATH="/usr/bin:/bin"',],
-    command     => $secure_linux_cis::update_command,
-  }
+    exec { 'update-zypper' :
+      command   => 'zypper update',
+      path      => ['/usr/bin', '/usr/sbin',],
+      logoutput => true,
 }
