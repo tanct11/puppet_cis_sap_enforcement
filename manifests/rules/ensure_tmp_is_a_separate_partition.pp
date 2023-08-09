778c97b6a6d288238d9f406ceede72dea96ed27d
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 14:12:31 +0800

    Delete ensure_tmp_is_a_separate_partition.pp

--- manifests/rules/ensure_tmp_is_a_separate_partition.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure /tmp is a separate partition 
-#
-class secure_linux_cis::rules::ensure_tmp_is_a_separate_partition {
-  include secure_linux_cis::rules::ensure_tmp_is_configured
-}
