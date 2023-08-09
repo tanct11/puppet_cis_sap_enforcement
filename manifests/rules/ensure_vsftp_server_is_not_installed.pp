4ac40770a5d866ef336f2fd46491d2e9c660778f
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 14:43:11 +0800

    Delete ensure_vsftp_server_is_not_installed.pp

--- manifests/rules/ensure_vsftp_server_is_not_installed.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure VSFTP Server is not installed 
-#
-class secure_linux_cis::rules::ensure_vsftp_server_is_not_installed {
-  package { 'vsftpd':
-    ensure => absent,
-  }
-}
