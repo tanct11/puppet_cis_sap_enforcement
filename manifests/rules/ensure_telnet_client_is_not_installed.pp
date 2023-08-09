ca0c243bf3b9655d6dfedba8cfbbfbacdb65d00a
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 13:36:00 +0800

    Update ensure_telnet_client_is_not_installed.pp

--- manifests/rules/ensure_telnet_client_is_not_installed.pp
+++ manifests/rules/ensure_telnet_client_is_not_installed.pp
@@ -3,16 +3,7 @@
 # @summary Ensure telnet client is not installed 
 #
 class secure_linux_cis::rules::ensure_telnet_client_is_not_installed {
-  case $facts['osfamily'] {
-    'Suse': {
       package { 'telnet':
         ensure   => absent,
-      }
-    }
-    default: {
-      package { 'telnet':
-        ensure   => absent,
-      }
-    }
   }
 }
