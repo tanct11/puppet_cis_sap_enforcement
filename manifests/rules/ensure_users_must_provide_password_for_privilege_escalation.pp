2cc4e3a07c50cf2b367543adbc59f14d744bbdc6
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 14:34:47 +0800

    Delete ensure_users_must_provide_password_for_privilege_escalation.pp

--- manifests/rules/ensure_users_must_provide_password_for_privilege_escalation.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure users must provide password for privilege escalation 
-#
-class secure_linux_cis::rules::ensure_users_must_provide_password_for_privilege_escalation {
-  # TODO
-}
