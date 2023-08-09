a50b1c597e997b035ce3b7331b3353d0193149f0
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 13:29:26 +0800

    Update ensure_systemd_timesyncd_is_configured.pp

--- manifests/rules/ensure_systemd_timesyncd_is_configured.pp
+++ manifests/rules/ensure_systemd_timesyncd_is_configured.pp
@@ -3,9 +3,13 @@
 # @summary Ensure systemd-timesyncd is configured 
 #
 class secure_linux_cis::rules::ensure_systemd_timesyncd_is_configured {
-  if $secure_linux_cis::time_sync == 'systemd-timesyncd' {
-    file { '/etc/systemd/timesyncd.conf':
-      ensure => file,
-    }
+    file_line { '/etc/systemd/timesyncd.conf':
+      ensure => present,
+      line   => "#NTP=
+                #FallbackNTP=time1.google.com time2.google.com time3.google.com time4.google.com
+                #RootDistanceMaxSec=5
+                #PollIntervalMinSec=32
+                #PollIntervalMaxSec=2048",
+      path  => '/etc/systemd/timesyncd.conf',
   }
 }
