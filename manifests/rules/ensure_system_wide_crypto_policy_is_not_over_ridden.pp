6b4e5de2fa30d6bf5753532f664b3a420654fb48
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 13:22:17 +0800

    Delete ensure_system_wide_crypto_policy_is_not_over_ridden.pp

--- manifests/rules/ensure_system_wide_crypto_policy_is_not_over_ridden.pp
+++
@@ -1,9 +0,0 @@
-# @api private
-#
-# @summary Ensure system-wide crypto policy is not over-ridden 
-#
-class secure_linux_cis::rules::ensure_system_wide_crypto_policy_is_not_over_ridden {
-  unless $facts['crypto_policy_sshd'] {
-    warning('System-wide Crypto policy for sshd over-ridden.')
-  }
-}
