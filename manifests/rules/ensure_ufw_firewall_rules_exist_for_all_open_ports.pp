6b78b446554b6ef67f465d7362d686cf6340a9ba
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-09 14:15:35 +0800

    Delete ensure_ufw_firewall_rules_exist_for_all_open_ports.pp

--- manifests/rules/ensure_ufw_firewall_rules_exist_for_all_open_ports.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure ufw firewall rules exist for all open ports 
-#
-class secure_linux_cis::rules::ensure_ufw_firewall_rules_exist_for_all_open_ports {
-  # TODO
-}
