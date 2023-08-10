0ffb37991e72fd888baf743be33f8129e9c4bddf
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-10 14:34:29 +0800

    Update ensure_core_dumps_are_restricted.pp

--- manifests/rules/ensure_core_dumps_are_restricted.pp
+++ manifests/rules/ensure_core_dumps_are_restricted.pp
@@ -4,7 +4,8 @@
 #
 class secure_linux_cis::rules::ensure_core_dumps_are_restricted {
   file_line { 'limits_hc':
-    path  => '/etc/security/limits.conf',
+    ensure => present,
+    path  => '/etc/sysctl.d/cis-hardening.conf',
     line  => '* hard core 0',
     match => '^* hard core ',
   }
@@ -17,11 +18,11 @@ class secure_linux_cis::rules::ensure_core_dumps_are_restricted {
   }
   
   exec { 'systemctl is-enabled coredump.service':
-    command   => "systemctl is-enabled coredump.service",
+    command   => "systemctl daemon-reload",
     path      => ['/usr/bin', '/usr/sbin',],
     logoutput => true,
   }
   sysctl { 'fs.suid_dumpable' :
-      value = 0,
+      value => 0,
       }
 }
