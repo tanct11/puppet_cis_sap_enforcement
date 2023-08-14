7773786731a1f5f6db09131265a02d576cf55a8c
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-14 17:07:51 +0800

    Update ensure_address_space_layout_randomization_aslr_is_enabled.pp

--- manifests/rules/ensure_address_space_layout_randomization_aslr_is_enabled.pp
+++ manifests/rules/ensure_address_space_layout_randomization_aslr_is_enabled.pp
@@ -3,11 +3,18 @@
 # @summary Ensure address space layout randomization (ASLR) is   enabled 
 #
 class secure_linux_cis::rules::ensure_address_space_layout_randomization_aslr_is_enabled {
-  file { '/etc/sysctl.d/60-kernel_sysctl.conf':
-    ensure => file,
-    source => 'puppet:///modules/secure_linux_cis/60-kernel_sysctl.conf',
+  file_line { '/etc/sysctl.d/60-kernel_sysctl.conf':
+    ensure => present,
+    match  => '^(kernel\.randomize_va_space).+',
+    line   => 'kernel.randomize_va_space = 2',
   }
-  -> exec { '/usr/sbin/sysctl -w kernel.randomize_va_space=2':
-    refreshonly => true,
+
+  file_line { '/etc/sysctl.d/cis-hardening.conf':
+    ensure => present,
+    match  => '^(kernel\.randomize_va_space).+',
+    line   => 'kernel.randomize_va_space = 2',
+  }
+  sysctl { 'kernel.randomize_va_space=2' :
+    value => 2,
   }
 }
