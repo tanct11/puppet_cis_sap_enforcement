10105f15819d894de4eab8f10e10b40fa43846b1
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-14 16:53:10 +0800

    Update ensure_access_to_the_su_command_is_restricted.pp

--- manifests/rules/ensure_access_to_the_su_command_is_restricted.pp
+++ manifests/rules/ensure_access_to_the_su_command_is_restricted.pp
@@ -3,14 +3,14 @@
 # @summary Ensure access to the su command is restricted 
 #
 class secure_linux_cis::rules::ensure_access_to_the_su_command_is_restricted {
-  group { 'su_group':
+  group { 'sugroup':
     ensure  => present,
     members => [],
   }
 
   file_line { 'su':
     path              => '/etc/pam.d/su',
-    line              => 'auth required pam_wheel.so use_uid group=su_group',
+    line              => 'auth required pam_wheel.so use_uid group=sugroup',
     match             => '^\s*auth\s+required\s+pam_wheel\.so\s+(\S+\s+)*use_uid\s+(\S+\s+)*group=\S+\ s*(\S+\s*)*(\s+#.*)?$',
     match_for_absence => true,
   }
