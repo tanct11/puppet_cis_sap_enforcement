8a2a0173973d65ebea650e93dff7940464596f9a
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-14 17:30:27 +0800

    Update ensure_all_groups_in_etc_passwd_exist_in_etc_group.pp

--- manifests/rules/ensure_all_groups_in_etc_passwd_exist_in_etc_group.pp
+++ manifests/rules/ensure_all_groups_in_etc_passwd_exist_in_etc_group.pp
@@ -2,8 +2,8 @@
 #
 # @summary Ensure all groups in /etc/passwd exist in /etc/group 
 #
-class secure_linux_cis::rules::ensure_all_groups_in_etc_passwd_exist_in_etc_group {
-  String $group,
+class secure_linux_cis::rules::ensure_all_groups_in_etc_passwd_exist_in_etc_group (
+  String $group){
   file_line { 'add missing group' :
     ensure => present,
     path   => '/etc/group',
