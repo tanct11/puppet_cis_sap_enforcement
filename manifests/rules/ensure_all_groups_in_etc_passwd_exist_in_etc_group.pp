2b4c6517f14c933150d5853d05ae431440a54b3b
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-14 18:07:48 +0800

    Update ensure_all_groups_in_etc_passwd_exist_in_etc_group.pp

--- manifests/rules/ensure_all_groups_in_etc_passwd_exist_in_etc_group.pp
+++ manifests/rules/ensure_all_groups_in_etc_passwd_exist_in_etc_group.pp
@@ -1,12 +1,21 @@
 # @api private
 #
-# @summary Ensure all groups in /etc/passwd exist in /etc/group 
+# @summary Ensure all groups in /etc/passwd exist in /etc/group
 #
-class secure_linux_cis::rules::ensure_all_groups_in_etc_passwd_exist_in_etc_group (
-  String $group){
-  file_line { 'add missing group' :
-    ensure => present,
-    path   => '/etc/group',
-    line   => $group, 
-  }
+class test {
+        exec { 'Add missing groups to ':
+          command   => 'for i in $(cut -s -d: -f4 /etc/passwd | sort -u ); do
+                        grep -q -P "^.*?:[^:]*:$i:" /etc/group
+                        if [ $? -ne 0 ]; then
+                                echo "Group $i is referenced by /etc/passwd but does not exist in /etc/group"
+                                echo "Adding missing groups to /etc/group"
+                                echo $i >> /etc/group
+                        else
+                                echo "VM is compliant for this control"
+                        fi
+                done',
+          path      => ['/bin', '/sbin' , '/usr/bin', '/usr/sbin'],
+          logoutput => true,
+          provider  => 'shell',
+      }
 }
