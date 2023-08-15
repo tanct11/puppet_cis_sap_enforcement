c67053f7e78f5df200a044bf60b14ea3856a684f
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-15 22:15:17 +0800

    Update ensure_all_users_last_password_change_date_is_in_the_past.pp

--- manifests/rules/ensure_all_users_last_password_change_date_is_in_the_past.pp
+++ manifests/rules/ensure_all_users_last_password_change_date_is_in_the_past.pp
@@ -3,15 +3,12 @@
 # @summary Ensure all users last password change date is in the past 
 #
 class secure_linux_cis::rules::ensure_all_users_last_password_change_date_is_in_the_past {
-  # local_users fact may be undef
-  $local_users = pick($facts['local_users'], {})
-  $local_users.each |String $user, Hash $attributes| {
-    unless $attributes['password_date_valid'] {
-      # fail("User ${user} has a password last changed date in the future. Please investigate.")
-      notify { "plcd ${user}":
-        message  => 'Not in compliance with CIS  (Scored). We believe the user has a password last changed date in the future. Please investigate.', #lint:ignore:140chars
-        loglevel => 'warning',
-      }
-    }
-  }
+  exec { 'ensure password change is in the past':
+    command   => ' for usr in $(cut -d: -f1 /etc/shadow); 
+                   do 
+                     [[ $(chage --list $usr | grep '^Last password change' | cut -d: -f2) > $(date) ]] && echo "$usr :$(chage --list $usr | grep '^Last password change' | cut -d: -f2)";echo ""; 
+                  done',
+    path      => ['/bin', '/sbin' , '/usr/bin', '/usr/sbin'],
+    logoutput => true,
+    provider  => 'shell',    
 }
