58add8908d5700da33c8123694192e287bb8c918
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-15 23:00:35 +0800

    Update ensure_base_chains_exist.pp

--- manifests/rules/ensure_base_chains_exist.pp
+++ manifests/rules/ensure_base_chains_exist.pp
@@ -2,4 +2,12 @@
 #
 # @summary Ensure base chains exist 
 #
-class secure_linux_cis::rules::ensure_base_chains_exist {}
+class secure_linux_cis::rules::ensure_base_chains_exist {
+  exec { 'base chains' :
+    command => 'nft create chain inet filter input { type filter hook input priority 0 \; };
+                nft create chain inet filter forward { type filter hook forward priority 0 \; }
+                nft create chain inet filter output { type filter hook output priority 0 \; }',
+    path      => ['/bin', '/sbin' , '/usr/bin', '/usr/sbin'],
+    logoutput => true,
+    provider  => 'shell',     
+}
