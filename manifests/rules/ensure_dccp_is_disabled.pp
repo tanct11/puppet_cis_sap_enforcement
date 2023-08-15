1f1d3d95d7fa5550ba4277af06f9c6e165670507
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-15 18:14:46 +0800

    Update ensure_dccp_is_disabled.pp

--- manifests/rules/ensure_dccp_is_disabled.pp
+++ manifests/rules/ensure_dccp_is_disabled.pp
@@ -3,13 +3,11 @@
 # @summary Ensure DCCP is disabled 
 #
 class secure_linux_cis::rules::ensure_dccp_is_disabled {
-  realize File['/etc/modprobe.d/storage_disable.conf']
 
   file_line { 'Disable dccp':
     ensure  => present,
-    path    => '/etc/modprobe.d/storage_disable.conf',
-    line    => 'install usb-storage /bin/true',
-    match   => '^install\s+usb-storage',
-    require => File['/etc/modprobe.d/storage_disable.conf'],
+    path    => '/etc/modprobe.d/dccp.conf',
+    line    => 'install dccp /bin/true',
+    match   => '^install dccp/gi',
   }
 }
