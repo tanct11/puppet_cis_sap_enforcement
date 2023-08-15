f151a1ff6678d372dee2e023bbddebb68ee06a63
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-15 16:33:35 +0800

    Update ensure_SSH_MaxAuthTries_is_set_to_4_or_less.pp

--- manifests/rules/ensure_SSH_MaxAuthTries_is_set_to_4_or_less.pp
+++ manifests/rules/ensure_SSH_MaxAuthTries_is_set_to_4_or_less.pp
@@ -2,7 +2,7 @@ class secure_linux_cis::rules::ensure_SSH_MaxAuthTries_is_set_to_4_or_less {
         file_line { 'ensure_SSH_MaxAuthTries_is_set_to_4_or_less' :
                 ensure => present,
                 path   => '/etc/ssh/sshd_config',
-                line   => 'maxauthtries 4',
-                match  => "^(maxauthtries.+$)",
+                line   => 'MaxAuthTries 4',
+                match  => "^(MaxAuthTries.+$)",
   }
 }
