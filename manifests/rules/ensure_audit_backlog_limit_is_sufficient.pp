612bd77f6294538012ae89a117fa3473ba1ba637
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-15 16:23:12 +0800

    Update ensure_audit_backlog_limit_is_sufficient.pp

--- manifests/rules/ensure_audit_backlog_limit_is_sufficient.pp
+++ manifests/rules/ensure_audit_backlog_limit_is_sufficient.pp
@@ -4,7 +4,7 @@
 #
 class secure_linux_cis::rules::ensure_audit_backlog_limit_is_sufficient {
 
-   exec { 'Set permissions on all existing log files in /var/log':
+   exec { 'backlog limit is sufficient:
     command   => "sed -E 's/(audit_backlog_limit=?)(\S+)/audit_backlog_limit=32768/g' /boot/grub2/grub.cfg;
                   grub2-mkconfig -o /boot/grub2/grub.cfg",
     path      => ['/usr/bin', '/usr/sbin',],
