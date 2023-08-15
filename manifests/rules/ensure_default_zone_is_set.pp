4e604e69137ff20d713cd22f7f87f12fce093ffc
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-15 23:04:37 +0800

    Delete ensure_default_zone_is_set.pp

--- manifests/rules/ensure_default_zone_is_set.pp
+++
@@ -1,7 +0,0 @@
-# @api private
-#
-# @summary Ensure default zone is set 
-#
-class secure_linux_cis::rules::ensure_default_zone_is_set {
-  # Handled in secure_linux_cis::rules::ensure_firewalld_is_installed
-}
