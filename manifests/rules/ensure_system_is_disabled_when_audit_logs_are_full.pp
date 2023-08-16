b43dec2b68a1a70bf2edee725eaf1e3ebdce213e
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-16 20:38:47 +0800

    Update ensure_system_is_disabled_when_audit_logs_are_full.pp

--- manifests/rules/ensure_system_is_disabled_when_audit_logs_are_full.pp
+++ manifests/rules/ensure_system_is_disabled_when_audit_logs_are_full.pp
@@ -8,18 +8,24 @@ class secure_linux_cis::rules::ensure_system_is_disabled_when_audit_logs_are_ful
     ensure => present,
     path   => '/etc/audit/auditd.conf',
     line   => 'space_left_action = email',
-    match  => '^space_left_action',
+    match  => '(?i)^space_left_action',
+    multiple                              => true,
+    replace_all_matches_not_matching_line => true,
   }
   file_line { 'action_mail_acct':
     ensure => present,
     path   => '/etc/audit/auditd.conf',
     line   => 'action_mail_acct = root',
-    match  => '^action_mail_acct',
+    match  => '(?i)^action_mail_acct',
+     multiple                              => true,
+    replace_all_matches_not_matching_line => true,
   }
   file_line { 'admin_space_left_action':
     ensure => present,
     path   => '/etc/audit/auditd.conf',
     line   => 'admin_space_left_action = halt',
-    match  => '^admin_space_left_action',
+    match  => '(?i)^admin_space_left_action',
+    multiple                              => true,
+    replace_all_matches_not_matching_line => true,
   }
 }
