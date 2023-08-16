752db60aca4a33bde71ad2386cc56be5cc582396
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-16 20:16:33 +0800

    Update ensure_ssh_logingracetime_is_set_to_one_minute_or_less.pp

--- manifests/rules/ensure_ssh_logingracetime_is_set_to_one_minute_or_less.pp
+++ manifests/rules/ensure_ssh_logingracetime_is_set_to_one_minute_or_less.pp
@@ -8,6 +8,8 @@ class secure_linux_cis::rules::ensure_ssh_logingracetime_is_set_to_one_minute_or
     ensure => 'present',
     path   => '/etc/ssh/sshd_config',
     line   => "LoginGraceTime 60",
-    match  => '^#?LoginGraceTime',
+    match  => '(?i)^#?LoginGraceTime',
+    multiple                              => true,
+    replace_all_matches_not_matching_line => true,
   }
 }
