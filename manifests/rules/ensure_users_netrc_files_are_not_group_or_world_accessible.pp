f29b830ddcbd581b9b9b66140d309ee1250d990b
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-16 16:01:32 +0800

    Update ensure_users_netrc_files_are_not_group_or_world_accessible.pp

--- manifests/rules/ensure_users_netrc_files_are_not_group_or_world_accessible.pp
+++ manifests/rules/ensure_users_netrc_files_are_not_group_or_world_accessible.pp
@@ -3,17 +3,20 @@
 # @summary Ensure users' .netrc Files are not group or world accessible 
 #
 class secure_linux_cis::rules::ensure_users_netrc_files_are_not_group_or_world_accessible {
-  #!/bin/bash 
-
-awk -F: '($1 !~ /^(root|halt|sync|shutdown)$/ && $7 != "'"$(which nologin)"'" && $7 != "/bin/false" && $7 != "/usr/bin/false") { print $1 " " $6 }' /etc/passwd | while read user dir; do
-        if [ ! -d "$dir" ]; then
-                echo "The home directory ($dir) of user $user does not exist." 
-        else
-                if [ ! -h "$dir/.netrc" -a -f "$dir/.netrc" ]; then
+  exec { 'netrc':
+    command => 'awk -F: \'($1 !~ /^(root|halt|sync|shutdown)$/ && $7 != "\'"$(which nologin)"\'" && $7 != "/bin/false" && $7 != "/usr/bin/false") { print $1 " " $6 }\' /etc/passwd | while read user dir; do
+                  if [ ! -d "$dir" ]; then
+                    echo "The home directory ($dir) of user $user does not exist." 
+                  else
+                    if [ ! -h "$dir/.netrc" -a -f "$dir/.netrc" ]; then
                         echo ".netrc file $dir/.netrc exists" 
                         echo "Apply actions according to company policy"
-                fi
-        fi
-done
+                    fi
+                  fi
+                done',
+    path      => ['/usr/bin', '/usr/sbin',],
+    logoutput => true,
+    provider  => 'shell',
 
+  }
 }
