627eaf4d60aa3ba9c4ecde9b24afe9f34aa0f128
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-16 18:37:16 +0800

    Update ensure_users_dot_files_are_not_group_or_world_writable.pp

--- manifests/rules/ensure_users_dot_files_are_not_group_or_world_writable.pp
+++ manifests/rules/ensure_users_dot_files_are_not_group_or_world_writable.pp
@@ -3,17 +3,28 @@
 # @summary Ensure users' dot files are not group or world writable 
 #
 class secure_linux_cis::rules::ensure_users_dot_files_are_not_group_or_world_writable {
-  file { '/usr/share/cis_scripts/dot_file_wr.sh':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0700',
-    content => file('secure_linux_cis/dot_file_wr.sh'),
-  }
-  unless $facts['dot_file_writable'].empty {
-    notify { 'dfw':
-      message  => 'Not in compliance with CIS 0 (Scored). There are DOT files that are either group or world writable. Check the dot_file_writable fact for details',#lint:ignore:140chars
-      loglevel => 'warning',
-    }
-  }
+  exec{ 'dot files are not group' :
+    command => 'grep -E -v \'^(halt|sync|shutdown)\' /etc/passwd | awk -F: \'($7 != "\'"$(which nologin)"\'" && $7 != "/bin/false") { print $1 " " $6 }\' | while read user dir; do 
+                  if [ ! -d "$dir" ]; then 
+                    echo "The home directory ($dir) of user $user does not exist." 
+                  else 
+                    for file in $dir/.[A-Za-z0-9]*; do 
+                      if [ ! -h "$file" -a -f "$file" ]; then 
+                        fileperm=$(ls -ld $file | cut -f1 -d" ") 
+            
+                        if [ $(echo $fileperm | cut -c6) != "-" ]; then 
+                          echo "Group Write permission set on file $file" 
+                          echo "Perform action depending on company policy"
+                        fi 
+                        if [ $(echo $fileperm | cut -c9) != "-" ]; then 
+                          echo "Other Write permission set on file $file" 
+                          echo "Perform action depending on company policy"
+                        fi 
+                      fi 
+                  done 
+                fi 
+              done'
+    path      => ['/bin', '/sbin' , '/usr/bin', '/usr/sbin'],
+    logoutput => true,
+    provider  => 'shell',
 }
