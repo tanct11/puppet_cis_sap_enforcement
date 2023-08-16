70700458edfb3fd62099e4eb8a4611ed6e4c765c
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-16 23:07:20 +0800

    Update ensure_ssh_pam_is_enabled.pp

--- manifests/rules/ensure_ssh_pam_is_enabled.pp
+++ manifests/rules/ensure_ssh_pam_is_enabled.pp
@@ -8,6 +8,8 @@ class secure_linux_cis::rules::ensure_ssh_pam_is_enabled {
     ensure => present,
     path   => '/etc/ssh/sshd_config',
     line   => 'UsePam yes',
-    match  => '^\s*UsePam\i',
+    match  => '^(?i)\UsePam',
+    replace_all_matches_not_matching_line => true,
+    multiple                              => true,
   }
 }
