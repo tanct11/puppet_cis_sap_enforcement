193e8fb23e02844a0fd95b70cbee2d42a863fee9
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-16 20:44:41 +0800

    Update ensure_system_accounts_are_secured.pp

--- manifests/rules/ensure_system_accounts_are_secured.pp
+++ manifests/rules/ensure_system_accounts_are_secured.pp
@@ -7,13 +7,13 @@ class secure_linux_cis::rules::ensure_system_accounts_are_secured {
 
 
   exec { 'Set all system accounts to a non login shell':
-    command => 'awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"' && $7!="'"$(which nologin)"'" && $7!="/bin/false" && $7!="/usr/bin/false") {print $1}' /etc/passwd | while read -r user; do usermod -s "$(which nologin)" "$user"; done',
+    command => 'awk -F: \'($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $1!~/^\+/ && $3<\'"$(awk \'/^\s*UID_MIN/{print $2}\' /etc/login.defs)"\' && $7!="\'"$(which nologin)"\'" && $7!="/bin/false" && $7!="/usr/bin/false") {print $1}\' /etc/passwd | while read -r user; do usermod -s "$(which nologin)" "$user"; done',
     path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
   }
 
 
   exec { 'Lock not root system accounts':
-    command => 'awk -F: '($1!="root" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"') {print $1}' /etc/passwd | xargs -I '{}' passwd -S '{}' | awk '($2!="L" && $2!="LK") {print $1}' | while read -r user; do usermod -L "$user"; done',
+    command => 'awk -F: \'($1!="root" && $1!~/^\+/ && $3<\'"$(awk \'/^\s*UID_MIN/{print $2}\' /etc/login.defs)"\') {print $1}\' /etc/passwd | xargs -I \'{}\' passwd -S \'{}\' | awk \'($2!="L" && $2!="LK") {print $1}\' | while read -r user; do usermod -L "$user"; done',
     path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
   }
   
