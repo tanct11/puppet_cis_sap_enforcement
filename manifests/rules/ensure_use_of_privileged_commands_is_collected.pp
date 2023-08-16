1c4d60246806ed22d6f6c8eec307f453675843e7
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-16 19:29:57 +0800

    Update ensure_use_of_privileged_commands_is_collected.pp

--- manifests/rules/ensure_use_of_privileged_commands_is_collected.pp
+++ manifests/rules/ensure_use_of_privileged_commands_is_collected.pp
@@ -3,20 +3,12 @@
 # @summary Ensure use of privileged commands is collected 
 #
 class secure_linux_cis::rules::ensure_use_of_privileged_commands_is_collected {
-  file { '/usr/share/cis_scripts/audit_priv.sh':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0700',
-    content => file('secure_linux_cis/audit_priv.sh'),
-  }
-  unless $facts['audit_privileged'].empty {
-    file { '/etc/audit/rules.d/cis_4_1_12.rules':
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0600',
-      content => $facts['audit_privileged'],
+  exec { 'privileged commands are collected' :
+    command => 'command=$(find / -xdev \( -perm -4000 -o -perm -2000 \) -type f | awk \'{print "-a always,exit -F path=" $1 " -F perm=x -F auid>=\'"$(awk \'/^\s*UID_MIN/{print $2}\' /etc/login.defs)"\' -F auid!=4294967295 -k privileged" }\');
+                grep -qF -- "$command" /etc/audit/rules.d/cis_hardening.rules || echo "$command" >> /etc/audit/rules.d/cis_hardening.rules',
+    path      => ['/bin', '/sbin' , '/usr/bin', '/usr/sbin'],
+    logoutput => true,
+    provider  => 'shell',
     }
   }
-}
+
