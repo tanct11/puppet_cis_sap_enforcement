2c20fef18238682a22d21b481c2f6f0bc4e6d3c7
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-16 19:45:53 +0800

    Update ensure_audit_log_storage_size_is_configured.pp

--- manifests/rules/ensure_audit_log_storage_size_is_configured.pp
+++ manifests/rules/ensure_audit_log_storage_size_is_configured.pp
@@ -5,9 +5,11 @@
 class secure_linux_cis::rules::ensure_audit_log_storage_size_is_configured {
   # require secure_linux_cis::rules::ensure_auditd_service_is_enabled
   file_line { 'max_log_file':
-    ensure => present,
-    path   => '/etc/audit/auditd.conf',
-    line   => "max_log_file = 8",
-    match  => 'max_log_file.+',
+    ensure                               => present,
+    path                                 => '/etc/audit/auditd.conf',
+    line                                 => "max_log_file = 8",
+    match                                => '(?i)max_log_file.+',
+    multiple                              => true,
+    replace_all_matches_not_matching_line => true,
   }
 }
