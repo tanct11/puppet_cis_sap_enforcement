d17801875f9804c5f98faa66b056d63aa497d441
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-16 16:23:15 +0800

    Update ensure_users_own_their_home_directories.pp

--- manifests/rules/ensure_users_own_their_home_directories.pp
+++ manifests/rules/ensure_users_own_their_home_directories.pp
@@ -3,17 +3,20 @@
 # @summary Ensure users own their home directories 
 #
 class secure_linux_cis::rules::ensure_users_own_their_home_directories {
-  file { '/usr/share/cis_scripts/home_dir_own.sh':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0700',
-    content => file('secure_linux_cis/home_dir_own.sh'),
-  }
-  unless $facts['home_directory_owner'].empty {
-    notify { 'hdo':
-      message  => 'Not in compliance with CIS  (Scored). You have a home directory that is not owned by the matching user name. Check the home_directory_owner fact for details',#lint:ignore:140chars
-      loglevel => 'warning',
-    }
-  }
+ exec { 'user_owns_home_directory’ :
+                command =>'grep -E -v \’^(halt|sync|shutdown)\’ /etc/passwd | awk -F: \’($7 != “\’”$(which nologin)”\’” && $7 != "/bin/false") { print $1 " " $6 }\’ | while read user dir; do
+                                if [ ! -d "$dir" ]; then
+                                        echo "The home directory ($dir) of user $user does not exist."
+                                else
+                                        owner=$(stat -L -c "%U" "$dir")
+                                        if [ "$owner" != "$user" ]; then
+                                                echo "The home directory ($dir) of user $user is owned by $owner."
+                                                echo “Follow company policy for actions to be taken”
+                                        fi
+                                fi
+                        done',
+        path      => ['/usr/bin', '/usr/sbin',],
+        logoutput => true,
+        provider => ‘shell’,
+        }
 }
