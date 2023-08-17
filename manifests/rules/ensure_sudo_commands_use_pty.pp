c225e9363fee1346dbf8eb7e8291b1ba0021f494
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-17 22:22:36 +0800

    Update ensure_sudo_commands_use_pty.pp

--- manifests/rules/ensure_sudo_commands_use_pty.pp
+++ manifests/rules/ensure_sudo_commands_use_pty.pp
@@ -3,6 +3,15 @@
 # @summary Ensure sudo commands use pty 
 #
 class secure_linux_cis::rules::ensure_sudo_commands_use_pty {
+ file_line { 'sudo_rule_pry':
+    ensure => present,
+    path  => '/etc/sudoers',
+    line  => 'Defaults use_pty',
+    match => '^#?Defaults\s+use_pty',
+    match_for_absence => true,
+
+  }
+  
   file_line { 'sudo_rule_pry 2':
     ensure => present,
     path  => '/etc/sudoers.d/usepty ',
