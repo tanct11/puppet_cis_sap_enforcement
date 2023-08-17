0e0cfc0186e115902c051eafefb54ae85e0c68c2
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-17 17:38:06 +0800

    Update ensure_shadow_group_is_empty.pp

--- manifests/rules/ensure_shadow_group_is_empty.pp
+++ manifests/rules/ensure_shadow_group_is_empty.pp
@@ -5,7 +5,8 @@
 class secure_linux_cis::rules::ensure_shadow_group_is_empty {
   exec { 'shadow group is empty' :
          command => 'grep ^shadow:[^:]*:[^:]*:[^:]+ /etc/group;
-                                awk -F: \'($4 == "<shadow-gid>") { print }\' /etc/passwd',
+                     awk -F: \'($4 == "<shadow-gid>") { print }\' /etc/passwd,
+                     echo "Perform actions according to company policy"',
          path      => ['/usr/bin', '/usr/sbin',],
          logoutput => true,
          provider => 'shell',
