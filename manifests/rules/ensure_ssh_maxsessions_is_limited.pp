c913bc175a62600fca969bf5797ae8f0be69fef0
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-17 17:22:14 +0800

    Update ensure_ssh_maxsessions_is_limited.pp

--- manifests/rules/ensure_ssh_maxsessions_is_limited.pp
+++ manifests/rules/ensure_ssh_maxsessions_is_limited.pp
@@ -8,6 +8,8 @@ class secure_linux_cis::rules::ensure_ssh_maxsessions_is_limited {
     ensure => present,
     path   => '/etc/ssh/sshd_config',
     line   => "MaxSessions 10",
-    match  => '^\s*MaxSessions',
+    match  => '(?i)^\s*MaxSessions',
+    multiple                              => true,
+    replace_all_matches_not_matching_line => true,
   }
 }
