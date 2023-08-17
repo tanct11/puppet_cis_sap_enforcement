e9b29cbba9b4b840d4f00179c62868101727a460
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-17 21:34:08 +0800

    Update ensure_permissions_on_ssh_private_host_key_files_are_configured.pp

--- manifests/rules/ensure_permissions_on_ssh_private_host_key_files_are_configured.pp
+++ manifests/rules/ensure_permissions_on_ssh_private_host_key_files_are_configured.pp
@@ -3,7 +3,7 @@
 # @summary Ensure permissions on SSH private host key files are configured 
 #
 class secure_linux_cis::rules::ensure_permissions_on_ssh_private_host_key_files_are_configured {
-    exec { 'private_host_key_files :'
+    exec { 'private_host_key_files' :
       command => "find /etc/ssh -xdev -type f -name 'ssh_host_*_key' -exec chmod u-x,g-wx,o-rwx {} \;
                   find /etc/ssh -xdev -type f -name 'ssh_host_*_key' -exec chown root:ssh_keys {} \;",
       path      => ['/usr/bin', '/usr/sbin',],
