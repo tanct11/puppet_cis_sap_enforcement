eedfa6dad2e29706b8dfbc82597262b1c6dfabe6
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-17 17:36:28 +0800

    Update ensure_shadow_group_is_empty.pp

--- manifests/rules/ensure_shadow_group_is_empty.pp
+++ manifests/rules/ensure_shadow_group_is_empty.pp
@@ -3,10 +3,11 @@
 # @summary Ensure shadow group is empty 
 #
 class secure_linux_cis::rules::ensure_shadow_group_is_empty {
-  unless $facts['shadowgroup'].empty {
-    notify { 'shadow':
-      message  => 'Not in compliance with CIS 6.2.20 (Scored). Someone is a member of the group shadow. Check the shadowgroup fact for details',#lint:ignore:140chars
-      loglevel => 'warning',
-    }
+  exec { 'shadow group is empty' :
+         command => 'grep ^shadow:[^:]*:[^:]*:[^:]+ /etc/group;
+                                awk -F: \'($4 == "<shadow-gid>") { print }\' /etc/passwd',
+         path      => ['/usr/bin', '/usr/sbin',],
+         logoutput => true,
+         provider => 'shell',
   }
 }
