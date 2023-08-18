86be7d87eb56605c03f394d5a6caf52e6a305dc2
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-18 08:40:48 +0800

    Update ensure_permissions_on_all_logfiles_are_configured.pp

--- manifests/rules/ensure_permissions_on_all_logfiles_are_configured.pp
+++ manifests/rules/ensure_permissions_on_all_logfiles_are_configured.pp
@@ -4,7 +4,7 @@
 #
 class secure_linux_cis::rules::ensure_permissions_on_all_logfiles_are_configured (
   exec { 'secure_linux_cis::rules::ensure_permissions_on_all_logfiles_are_configured':
-    commmand  => "find /var/log -type f -exec chmod g-wx,o-rwx "{}" + -o -type d -exec chmod g-wx,o-rwx "{}" +",
+    command  => 'find /var/log -type f -exec chmod g-wx,o-rwx \'{}\' + -o -type d -exec chmod g-wx,o-rwx \'{}\' +',
     path      => ['/usr/bin', '/usr/sbin',],
     logoutput => true,
   }
