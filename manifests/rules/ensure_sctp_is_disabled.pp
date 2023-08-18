935bf4359519c7d366e66838e6eeb82b20c764e4
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-18 08:36:37 +0800

    Update ensure_sctp_is_disabled.pp

--- manifests/rules/ensure_sctp_is_disabled.pp
+++ manifests/rules/ensure_sctp_is_disabled.pp
@@ -6,7 +6,7 @@ class secure_linux_cis::rules::ensure_sctp_is_disabled {
 
   file_line { 'Disable sctp':
     ensure  => present,
-    path    => '/etc/modprobe.d/storage_disable.conf',
+    path    => '/etc/modprobe.d/sctp.conf',
     line    => 'install sctp /bin/true',
     match   => '(?i)^install\s+sctp',
     multiple                              => true,
