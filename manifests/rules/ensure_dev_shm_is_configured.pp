4da9b8289b25d8a7d36fdd4462b9546fd5a96573
tanct11 <122010882+tanct11@users.noreply.github.com>
2023-08-22 17:46:19 +0800

    Update ensure_dev_shm_is_configured.pp

--- manifests/rules/ensure_dev_shm_is_configured.pp
+++ manifests/rules/ensure_dev_shm_is_configured.pp
@@ -3,11 +3,19 @@
 # @summary Ensure /dev/shm is configured 
 #
 class secure_linux_cis::rules::ensure_dev_shm_is_configured {
-  $mount   = '/dev/shm'
-
-  mount { $mount:
-    device => $mount,
-    fstype => 'tmpfs',
-    atboot => true,
+  file_line { '/tmp options':
+    ensure                                => present,
+    path                                  => '/etc/fstab',
+    match                                 => '/dev/shm',
+    line                                  => 'tmpfs /dev/shm tmpfs defaults,noexec,nodev,nosuid 0 0',
+    replace_all_matches_not_matching_line => true,
+    multiple                              => true,
+    noop                                  => true,
+  }
+  exec { '/tmp commands mounting':
+    command   => 'mount -o remount,noexec,nodev,nosuid /dev/shm;',
+    provider  => 'shell',
+    path      => ['/usr/bin', '/usr/sbin',],
+    logoutput => true,
   }
 }
